349b72617eba7636032e5e4d70f184eb6420a73d
kleenerStern <miauuuau@gmail.com>
2022-04-01 17:54:53 +0200

    Teilaufgabe 2 Done

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -14,6 +15,17 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
+			Properties props = new Properties();
+			Session session = Session.getInstance(props,null);
+
+			MimeMessage msg = new MimeMessage(session);
+			msg.setFrom("testSender@localhost");
+			msg.setRecipients(Message.RecipientType.TO,"labrat@localhost");
+			msg.setSubject("TestSubject");
+			msg.setSentDate(new Date());
+			msg.setText("TestText");
+			Transport.send(msg);
+
 			// your code here
 		} catch (Exception e) {
 			e.printStackTrace();
