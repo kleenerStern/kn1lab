3ebf26e9bbdf2c135b695060c300b78166e3ec51
kleenerStern <miauuuau@gmail.com>
2022-04-02 20:03:22 +0200

    Teilaufgabe 3 + Spielereien fertig

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.util.Properties;
+import java.util.Scanner;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -15,16 +16,36 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			Properties props = new Properties();
-			Session session = Session.getInstance(props,null);
+
+			Scanner scanner = new Scanner(System.in);
+			Properties properties = new Properties();
+			Session session = Session.getInstance(properties,null);
 
 			MimeMessage msg = new MimeMessage(session);
-			msg.setFrom("testSender@localhost");
+			System.out.println("Input your data to contact labrat@localhost");
+			System.out.println("Username / Firstname:");
+			String from = scanner.nextLine();
+			while (from.contains(" ")){
+				System.err.println("No whitespaces allowed. Please try again.");
+				from = scanner.nextLine();
+			}
+			msg.setFrom(from + "@localhost");
+
 			msg.setRecipients(Message.RecipientType.TO,"labrat@localhost");
-			msg.setSubject("TestSubject");
-			msg.setSentDate(new Date());
-			msg.setText("TestText");
+
+			System.out.println("Subject:");
+			String subject = scanner.nextLine();
+			msg.setSubject(subject);
+
+			Date currentDate = new Date();
+			msg.setSentDate(currentDate);
+
+			System.out.println("Mail Text:");
+			String text = scanner.nextLine();
+			msg.setText(text);
+
 			Transport.send(msg);
+			System.out.println("Thank you, " + from + ", your mail has been sent to labrat@localhost at " + currentDate + ".");
 
 			// your code here
 		} catch (Exception e) {
