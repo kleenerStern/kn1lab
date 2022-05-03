e59294227fccb0b77443ff68c77bd12cec1d1739
Bastik00 <GI-16h7zqpy95asl83>
2022-05-03 21:59:55 +0200

    sender funktionsfähig. TODO: randfälle

--- versuch2/src/Sender.java
+++ versuch2/src/Sender.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.Timer;
 import java.time.Duration;
 import java.util.concurrent.*;
 
@@ -33,60 +34,94 @@ public class Sender {
     private void send() throws IOException {
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        String[] payloadString;
-        int seq = 0;
+        String[] words;
+        int seq;
         int ackNum = 0;
+        int exeptedAckNum;
         boolean ackFlag = false;
-   	//Text einlesen und in Worte zerlegen
+        int timeoutTime = 1000;
+        int wordIdx = 0;
+        String receivedString = "";
+   	    //Text einlesen und in Worte zerlegen
         userInput = scanner.nextLine();
         userInput += " EOT";
-        payloadString = userInput.split(" ");
+        words = userInput.split(" ");
         // Socket erzeugen auf Port 9998 und Timeout auf eine Sekunde setzen
-        DatagramSocket clientSocket = new DatagramSocket(9998);
-        clientSocket.setSoTimeout(1000);
+        DatagramSocket socket = new DatagramSocket(9998);
+        socket.setSoTimeout(timeoutTime);
         // Iteration über den Konsolentext
-        while (true) {
-            for (String word : payloadString) {
 
-                Packet packetOut = new Packet(seq, ackNum, ackFlag, word.getBytes());
+        while (wordIdx < words.length) {
 
-                // serialize Packet for sending
-                ByteArrayOutputStream b = new ByteArrayOutputStream();
-                ObjectOutputStream o = new ObjectOutputStream(b);
-                o.writeObject(packetOut);
-                byte[] buf = b.toByteArray();
+            seq = ackNum;
+            Packet packetOut = new Packet(seq, ackNum, ackFlag, words[wordIdx].getBytes());
 
-
-                // Paket an Port 9997 senden
-                InetAddress address = InetAddress.getByName("localhost");
-                DatagramPacket packet = new DatagramPacket(buf, buf.length,
-                        address, 9997);
-                clientSocket.send(packet);;
-
-                try {
-                    // Auf ACK warten und erst dann Schleifenzähler inkrementieren
-                    if (false) {
-                        throw new ClassNotFoundException("ClassNotFoundException");
-                    }
-                    if (false) {
-                        throw new SocketTimeoutException("SocketTimeoutException");
-                    }
+            // serialize Packet for sending
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            ObjectOutputStream o = new ObjectOutputStream(b);
+            o.writeObject(packetOut);
+            byte[] buf = b.toByteArray();
 
 
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (SocketTimeoutException e) {
-                    System.out.println("Receive timed out, retrying...");
+            // Paket an Port 9997 senden
+            InetAddress address = InetAddress.getByName("localhost");
+            DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                    address, 9997);
+            socket.send(packet);
+            exeptedAckNum = seq + words[wordIdx].length();
+            try {
+                // Auf ACK warten und erst dann Schleifenzähler inkrementieren
+                byte[] bufIn = new byte[256];
+                DatagramPacket rcvPacketRaw = new DatagramPacket(bufIn, bufIn.length);
+                socket.receive(rcvPacketRaw);
+                TimeUnit.MILLISECONDS.sleep(timeoutTime);
+                // deserialize Packet
+                ObjectInputStream is = new ObjectInputStream(new ByteArrayInputStream(rcvPacketRaw.getData()));
+                Packet packetIn = (Packet) is.readObject();
+                String payload = new String(packetIn.getPayload());
+                /*
+                System.out.println("Received packet");
+                System.out.println("Sequence: " + packetIn.getSeq());
+                System.out.println("Ack: " + packetIn.getAckNum());
+                System.out.println("IsAckFlag: " + packetIn.isAckFlag());
+                System.out.println("Payload: " + payload);
+                */
+
+                if(payload.equals("EOT")) {
+                    System.out.println("Received all packages\nReceived String: " + receivedString);
+                    break;
                 }
+                // kein Paket angekommen
+                if(packetIn == null) {
+                    throw new SocketTimeoutException("SocketTimeoutException");
+                }
+                // uebertragung erfolgreich
+                if(packetIn.isAckFlag() && exeptedAckNum == packetIn.getAckNum()) {
+                    ackNum = packetIn.getAckNum();
+                    receivedString += payload + " ";
+                    wordIdx++;
+                }
+
+                // TODO: if (packetIn.isAckFlag() == false)
+
+                if (false) {
+                    throw new ClassNotFoundException("ClassNotFoundException");
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SocketTimeoutException e) {
+                System.out.println("Receive timed out, retrying...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            break;
+
         }
         
         // Wenn alle Packete versendet und von der Gegenseite bestätigt sind, Programm beenden
-        clientSocket.close();
+        socket.close();
         
         if(System.getProperty("os.name").equals("Linux")) {
-            clientSocket.disconnect();
+            socket.disconnect();
         }
 
         System.exit(0);
